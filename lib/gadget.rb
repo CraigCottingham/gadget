70a4a682f50e4135f5c5069984836683178707df
Craig S. Cottingham <craig@nexushq.com>
2014-01-27 10:33:00 -0600

    Rework methods in Gadget module to return table names instead of OIDs

--- lib/gadget.rb
+++ lib/gadget.rb
@@ -17,30 +17,46 @@ module Gadget
 
   def self.tables(conn)
     rs = conn.exec("SELECT c.oid, t.tablename FROM pg_tables t INNER JOIN pg_class c ON c.relname=t.tablename WHERE t.schemaname='public' ORDER BY t.tablename")
-    tuples = rs.map { | row | row }
+    tuples = rs.reduce({}) { | h, row | h[row['tablename']] = { :oid => row['oid'] }; h }
     rs.clear
     tuples
   end
 
-  def self.foreign_keys(conn)
-    rs = conn.exec("SELECT conrelid, confrelid FROM pg_constraint WHERE contype='f'")
-    tuples = rs.map { | row | row }
+  def self.foreign_keys(conn, tablename = nil)
+    sql = <<-END_OF_SQL
+SELECT t1.tablename AS tablename, t2.tablename AS refname
+FROM pg_constraint
+INNER JOIN pg_class c1 ON pg_constraint.conrelid=c1.oid
+INNER JOIN pg_tables t1 ON c1.relname=t1.tablename
+INNER JOIN pg_class c2 ON pg_constraint.confrelid=c2.oid
+INNER JOIN pg_tables t2 ON c2.relname=t2.tablename
+WHERE t1.schemaname='public'
+AND t2.schemaname='public'
+AND pg_constraint.contype='f'
+    END_OF_SQL
+    if tablename.nil?
+      rs = conn.exec(sql)
+    else
+      sql += " AND t1.tablename=$1"
+      rs = conn.exec_params(sql, [ tablename ])
+    end
+    tuples = rs.reduce({}) { | h, row | h[row['tablename']] ||= { :refs => [] }; h[row['tablename']][:refs] << row['refname']; h }
     rs.clear
     tuples
   end
 
-  def self.tables_in_dependency_order(dbname)
-    conn = PG::Connection.open(:dbname => dbname)
-    tables = self.tables(conn).reduce({}) { | h, tuple | h[tuple['oid']] = { :tablename => tuple['tablename'] }; h }
-    foreign_keys = self.foreign_keys(conn).reduce({}) do | h, tuple |
-      h[tuple['conrelid']] ||= []
-      h[tuple['conrelid']] << tuple['confrelid']
+  def self.tables_in_dependency_order(conn)
+    tables = self.tables(conn)
+    foreign_keys = self.foreign_keys(conn)
+    deps = tables.keys.inject(TsortableHash.new) do | h, k |
+      if foreign_keys.has_key?(k)
+        h[k] = foreign_keys[k][:refs]
+      else
+        h[k] = []
+      end
       h
     end
-    foreign_keys.each { | k, v | tables[k][:refs] = v.map { | oid | tables[oid][:tablename] } }
-    tables = tables.inject(TsortableHash.new) { | h, (k, v) | h[v[:tablename]] = v[:refs]; h }
-    conn.close
-    tables.tsort
+    deps.tsort
   end
 
 end
